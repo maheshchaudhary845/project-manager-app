4b2d62839714c374f12919f02b9c12eafe0347b8
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-20 14:04:37 +0530

    fix import route of AuthProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/authContext';
+import { AuthProvider } from './context/AuthContextuthContext';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './pages/Login';
 import Register from './pages/Register';
